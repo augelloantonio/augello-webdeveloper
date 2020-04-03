43d4e029abefd4695d1e8ee9df158e19ba88779a
gello94 <gello94@live.it>
2020-04-03 11:29:20 +0200

    changed data-src into src

--- assets/vendor/js/lazyload.js
+++ assets/vendor/js/lazyload.js
@@ -9,7 +9,9 @@ Add this script to end of page
 <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/lozad/dist/lozad.min.js"></script> 
 
 
-add lozad class to img elements
+add lozad class to img elements and use data-src instead of src
+<img data-src="assets/images/developer.jpg" class="w-100 h-100 " alt="website">
+
 
 
 */
